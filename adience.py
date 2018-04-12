16b299c56eeb24b2655fa266493d8e5fa2978e58
WonTiK <kws7894@naver.com>
2018-04-12 17:15:14 +0900

    Update adience.py

--- adience.py
+++ adience.py
@@ -10,7 +10,7 @@ import adience_input
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_integer('batch_size', 128, """Number of images to process in a batch.""")
-tf.app.flags.DEFINE_string('data_dir', './adience_data', """Path to the CIFAR-10 data directory.""")
+tf.app.flags.DEFINE_string('data_dir', './adience_data', """Path to the ADIENCE data directory.""")
 tf.app.flags.DEFINE_boolean('use_fp16', False, """Train the model using fp16.""")
 
 IMAGE_SIZE = adience_input.IMAGE_SIZE
